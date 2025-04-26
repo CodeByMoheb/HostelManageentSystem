ac14e2f809e7a9edaef0df335057c66bde85f058
Md. Mohebullah <mohebullah.cse@gmail.com>
2025-04-26 22:00:08 +0600

    Initial commit

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace HostelManageentSystem.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
